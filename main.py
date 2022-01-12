4515cca8cbd469913b98e74d2bf9a8108b3cbbf1
pdambekalns <97154866+pdambekalns@users.noreply.github.com>
2022-01-12 07:33:07 +0000

    izpildic 3 uzd

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-"""import kvard
-diag=float(input("ievadi kvadrāta diagonāles garumu! "))
-print("Kvadrāta laukums ir", kvard.laukums(diag))"""
-import virkne
-pirm=int(input("1.skaitlis:"))
-otr=int(input("2.skaitlis:"))
-n=int(input("n-tais elem.:"))
-print("Skaitlis ir:",virkne.algoritms(pirm,otr,n))
+import stabins
+skaitlis=input("Ievadi daudzciparu skaitli! ")
+stabins.total(skaitlis)
