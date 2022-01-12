db5633834ce5781832fbcfb5f7ba7fb65e40d577
Klaudija Roke <97155837+KlaudijaR@users.noreply.github.com>
2022-01-12 07:33:22 +0000

    izpildiju 3.uzdevumu

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import elementi
+"""import elementi
 x = int(input("ievadi skaitli: "))
 y = int(input("ievadi skaitli: "))
 n = int(input("ievadi skaitli: "))
-print(elementi.virkne(x,y,n))
+print(elementi.virkne(x,y,n))"""
+
+#3.uzdevums
+import stabins
+skaitlis = input("Ievadi daudzciparu skaitli: ")
+stabins.total(skaitlis)
---
+++ stabins.py
@@ -0,0 +1,8 @@
+def total(x):
+  z = 0
+  for i in range(len(x)):
+    y = x[i:]
+    print(" "*i + y)
+    z += int(y)
+  print("_"*len(x))
+  print(z)
