7872d76f465d1042d69840c30929b749af18bdc6
Matiss Melderis <42517419+DigitalConfusion@users.noreply.github.com>
2022-01-12 07:15:05 +0000

    Izpildīju 3.uzdevumu

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import mod
-
+"""
+# 2.uzdevums
 a1 = int(input("Ievadi 1.elementu: "))
 a2 = int(input("Ievadi 1.elementu: "))
 n = int(input("Ievadi kuru elementu atrast: "))
 print(mod.n_elements(a1, a2, n))
+"""
+# 3.uzdevums
+n = input("Ievadi skaitli, bez nullēm: ")
+kopa = 0
+for i in range(len(n)):
+  kopa += int(n[i:])
+  print(" " * i + n[i:])
+print("-" * len(n))
+print(kopa)
