3ef2e98cfc8143da2030925bfd21287f21bb15d1
Rojs Smits <97155747+RojsSmits@users.noreply.github.com>
2022-01-12 07:51:22 +0000

    iesāku.4.

--- cipars.py
+++ cipars.py
@@ -1,15 +1,13 @@
 def cip(n):
-  t=[(str(n)).split]
+  t=n.split( )
   print(t)
   g=[]
   for i in range (0,len(t)):
-    print(i)
     if i==1 and t[i]==0:
       continue
     if int(t[i-1])<int(t[i]):
       g.append(t[i-1])
     else:
       g.append(t[i])
-      print(t[i])
     return g
 
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+#uzd=("2.uzd")
 #import skaitlis
 #a=int(input("ievadi 1. skaitli "))
 #b=int(input("ievadi 2. skaitli "))
 #n=int(input("ievadi aprēķināmo kārtas skaitli ")
 #print(skaitlis.cipars(a,b,n))
-#import cipars 
-#n=int(input("ievadi skaitli "))
-#print(cipars.cip(n))
-import stabins
-t=str(input("ievadi daudzciparu skaitli "))
-print(stabins.numurs(t))
+#uzd=("4.uzd")
+import cipars 
+n=str(input("ievadi vairāku ciparu skaitli "))
+cipars.cip(n)
+#uzd=("3.uzd")
+#import stabins
+#t=str(input("ievadi daudzciparu skaitli "))
+#print(stabins.numurs(t))
