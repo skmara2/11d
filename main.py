888fd1802ceac5d4574244200ac08450f9d327b2
Daniels Gaidamovics <97155951+DanielsGaidamovics@users.noreply.github.com>
2022-01-12 07:32:43 +0000

    Izpildiju uzdevumu

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import skaitlis
-a=int(input("Ievadi virknes 1. elementu: "))
-b=int(input("Ievadi virknes 2. elementu: "))
-n=int(input("Ievadi skaitli līdz kuram turpināt: "))
-skaitlis.elem(a,b,n)
+import stabins
+skaitlis=input("Ievadi daudzciparu skaitli: ")
+stabins.total(skaitlis)
